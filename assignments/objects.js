fb0e4c092329c838302645a5d3d0c294c247fc72
MarquesC19 <Carter234>
2019-09-16 17:23:07 -0500

    working on Functions and conversations and objects JS still not completed with assignments yet

--- assignments/objects.js
+++ assignments/objects.js
@@ -3,14 +3,14 @@
 // ==== Challenge 1: Writing Objects ==== 
 // HR needs some information on the new interns put into a database.  Given an id, email, first name, and gender. Create an object for each person in the company list:
 
-// 1,mmelloy0@psu.edu,Mitzi,F
-// 2,kdiben1@tinypic.com,Kennan,M
-// 3,kmummery2@wikimedia.org,Keven,M
-// 4,gmartinson3@illinois.edu,Gannie,M
-// 5,adaine5@samsung.com,Antonietta,F
+1,const data=mmelloy0@psu.edu,Mitzi,F
+2,kdiben1@tinypic.com,Kennan,M
+3,kmummery2@wikimedia.org,Keven,M
+4,gmartinson3@illinois.edu,Gannie,M
+5,adaine5@samsung.com,Antonietta,F
 
 // Example format of an intern object: 1,examples@you.edu,Example,F
-const example = {
+let example = {
   "id": 0,
   "name": "Example",
   "email": "examples@you.edu",
@@ -18,21 +18,37 @@ const example = {
 }
 
 // Write your intern objects here:
-
+const mitzi = { id: 1, email: 'mmelloy0@psu.edu', name: 'Mitzi', gender: 'F' }
+const kennan ={ id: 2,
+  email: 'kdiben1@tinypic.com',
+  name: 'Kennan',
+  gender: 'M' }
+const keven = { id: 3,
+  email: 'kmummery2@wikimedia.org',
+  name: 'Keven',
+  gender: 'M' }
+const gannie = { id: 4,
+  email: 'gmartinson3@illinois.edu',
+  name: 'Gannie',
+  gender: 'M' }
+const antonietta = { id: 5,
+  email: 'adaine5@samsung.com',
+  name: 'Antonietta',
+  gender: 'F' }
 
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
-
+console.log(Mitzi.name);
 // Kennan's ID
-
+console.log(Kennan.id);
 // Keven's email
-
+console.log(Kevins.email);
 // Gannie's name
-
+console.log(Gannies.name);
 // Antonietta's Gender
-
+console.log(Antonietta.gender)
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
